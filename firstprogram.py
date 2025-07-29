1aa93d9ec8dc5831991bcac98a9b31d72dab71c8
Sangay-Kenchap <sangayrexy@gmail.com>
2025-07-29 14:49:19 +0600

    updated

--- firstprogram.py
+++ firstprogram.py
@@ -1,2 +1,13 @@
-print("Hello SWE!")
+'''print("Hello SWE!")
 print("college of science and technology")
+a=2
+b=3
+sum=a+b
+print("the sum of a and b is: ",sum)
+name:"Tashi"
+age:16'''
+name=input("Enter your name: ")
+age=input("Enter your age: ")
+print("you are",name , "and you are", age ,"years old")
+
+
